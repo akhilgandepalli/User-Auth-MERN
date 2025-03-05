46b9bc6ff38df3ae752d988fb25be9e76c21827c
akhilgandepalli <akhilgandepalli@gmail.com>
2025-03-05 14:02:38 +0530

    updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Outlet, Link } from 'react-router-dom'
 const Header = () => {
   return (
     <><header>
-        <h2>User Auth</h2>
+        <h2>User Authentication</h2>
         <ul>
             <Link key='register' to='/register'><li>Register</li></Link>
             <Link key='login' to='/login'><li>Login</li></Link>
